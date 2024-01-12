f5ac4f889d766a8d622084a33c0fbad48cc9418c
Oskar Modig <accounts@oskarmodig.com>
2024-01-12 16:39:26 +0100

    cleaning for windows only code

--- linux-scripts/setup.sh
+++ linux-scripts/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Comment out lando excludes
-#execute_part "excludes-disable"
-
 # Start lando
 lando start
 
@@ -14,10 +11,3 @@ execute_part "create-htaccess"
 
 # Create an empty debug.log file
 touch "/$MOD_LOC_WORDPRESS_PATH/wp-content/debug.log"
-
-# Restore lando excludes
-#execute_part "excludes-re-enable"
-# TODO: The rebuilding after this does not seem to include the "excludes". What can we do about this? Maybe wait/pause?
-
-# Rebuild lando
-lando rebuild -y
