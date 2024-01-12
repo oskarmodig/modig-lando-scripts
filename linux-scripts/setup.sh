203bc4ae969f481e869fd1e996952270f2ff6c76
Oskar Modig <accounts@oskarmodig.com>
2024-01-12 16:38:43 +0100

    rename htaccess file

--- linux-scripts/setup.sh
+++ linux-scripts/setup.sh
@@ -10,7 +10,7 @@ lando start
 execute_part "install-wordpress"
 
 # Create .htaccess file with contents
-execute_part "htaccess"
+execute_part "create-htaccess"
 
 # Create an empty debug.log file
 touch "/$MOD_LOC_WORDPRESS_PATH/wp-content/debug.log"
