5b7527d30bb677ef9c9cde2eef5219f4fd090aa6
Oskar Modig <accounts@oskarmodig.com>
2024-01-12 09:17:36 +0100

    lando excludes debugging

--- linux-scripts/parts/setup/excludes-re-enable.sh
+++ linux-scripts/parts/setup/excludes-re-enable.sh
@@ -5,3 +5,6 @@ if [ "$MOD_LOC_EXCLUDES_SECTION_EXISTS" -ne 0 ]; then
   echo_progress "Re-enabling lando excludes"
   sed -i '/^# excludes:/,/^# [^ ]/ s/^# //' "$MOD_LOC_LANDO_FILE"
 fi
+
+TEST_TEST=$(grep -c "^excludes:" "$MOD_LOC_LANDO_FILE")
+echo_progress "Test: $TEST_TEST"
--- linux-scripts/setup.sh
+++ linux-scripts/setup.sh
@@ -17,6 +17,7 @@ touch "/$MOD_LOC_WORDPRESS_PATH/wp-content/debug.log"
 
 # Restore lando excludes
 execute_part "excludes-re-enable"
+# TODO: The rebuilding after this does not seem to include the "excludes". What can we do about this? Maybe wait/pause?
 
 # Rebuild lando
 lando rebuild -y
