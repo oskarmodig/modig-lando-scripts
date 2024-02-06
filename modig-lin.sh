81b8e4acac7dd50d215cf97046af7b402497092d
Oskar Modig <accounts@oskarmodig.com>
2024-02-06 11:36:49 +0100

    bugfix

--- modig-lin.sh
+++ modig-lin.sh
@@ -42,7 +42,6 @@ MOD_LOC_WORDPRESS_PATH="$MOD_LOC_SCRIPT_CALLED_FROM/wordpress"
 # MOD_LOC_LANDO_FILE="$MOD_LOC_SCRIPT_CALLED_FROM/.lando.yml"
 # . "$MOD_LOC_CURRENT_SCRIPT_DIR/helpers/load-lando-env-files.sh"
 # . "$MOD_LOC_SCRIPTS_BASE_DIR/helpers/load-lando-env-vars.sh"
-
-. "$MOD_LOC_SCRIPTS_BASE_DIR/helpers/parse-main-variables.sh"
+# . "$MOD_LOC_SCRIPTS_BASE_DIR/helpers/parse-main-variables.sh"
 
 . "$MOD_LOC_SCRIPTS_BASE_DIR/helpers/run-main-script.sh"
