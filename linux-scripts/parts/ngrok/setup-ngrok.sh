62cacc2dc248f73d66664fbe575b876eb98636e8
Oskar Modig <accounts@oskarmodig.com>
2024-04-02 12:41:52 +0200

    path fix

--- linux-scripts/parts/ngrok/setup-ngrok.sh
+++ linux-scripts/parts/ngrok/setup-ngrok.sh
@@ -3,4 +3,4 @@
 # Setup plugin for ngrok
 echo_progress "Installing WP-Ngrok-Local"
 
-lando ssh -c "mkdir -p \"$MOD_LOC_ABSOLUT_WP_PATH/wp-content/mu-plugins\" && ln -sf var/www/.composer/vendor/oskarmodig/lando-scripts/included-vendor/wp-ngrok-local/ngrok-local.php \"$MOD_LOC_ABSOLUT_WP_PATH/wp-content/mu-plugins/ngrok-local.php\""
+lando ssh -c "mkdir -p \"$MOD_LOC_ABSOLUT_WP_PATH/wp-content/mu-plugins\" && ln -sf /var/www/.composer/vendor/oskarmodig/lando-scripts/included-vendor/wp-ngrok-local/ngrok-local.php \"$MOD_LOC_ABSOLUT_WP_PATH/wp-content/mu-plugins/ngrok-local.php\""
