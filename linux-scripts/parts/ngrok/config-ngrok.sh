9adab6da3fd6b90ee3a58c2bb312b97189d6c524
Oskar Modig <accounts@oskarmodig.com>
2024-04-02 13:11:21 +0200

    ngrok config fix

--- linux-scripts/parts/ngrok/config-ngrok.sh
+++ linux-scripts/parts/ngrok/config-ngrok.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-call_wp config set set WP_LOCAL_NGROK_URL "$NGROK_URL"
+call_wp config set WP_LOCAL_NGROK_URL "$NGROK_URL"
 
 echo "ngrok url: $NGROK_URL"
 echo "ngrok web interface: http://localhost:4040"
