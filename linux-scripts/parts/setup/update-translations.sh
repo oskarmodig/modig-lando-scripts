a6b17eca387cdb0c6d713cba5b0bc06df8c82476
Oskar Modig <accounts@oskarmodig.com>
2024-04-16 17:30:24 +0200

    Update scripts for language installation and WooCommerce setup
    Added Swedish language installation for both themes and plugins in the update-translations script. In the setup script for WooCommerce, fixed the issue with onboarding profile skipping by updating the option with a json-formatted string.

--- linux-scripts/parts/setup/update-translations.sh
+++ linux-scripts/parts/setup/update-translations.sh
@@ -1,4 +1,6 @@
 echo_progress "Update translations"
+call_wp language plugin install --all sv_SE
+call_wp language theme install --all sv_SE
 call_wp language plugin update --all
 call_wp language theme update --all
 call_wp language core update
