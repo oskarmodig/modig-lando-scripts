c63c85fadde75539dbd331aab20e816dd395b50e
Oskar Modig <accounts@oskarmodig.com>
2024-05-23 12:11:15 +0200

    path fix

--- linux-scripts/make-strict.sh
+++ linux-scripts/make-strict.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Run php file
-PHP_SCRIPT_PATH=$MOD_LOC_CURRENT_SCRIPT_DIR/parts/make-stict/make-strict.php
+PHP_SCRIPT_PATH=$MOD_LOC_CURRENT_SCRIPT_DIR/parts/make-strict/make-strict.php
 php "$PHP_SCRIPT_PATH"
