ff0d6b42dcc3fac41ebdca8531a657d59dd3f529
Oskar Modig <accounts@oskarmodig.com>
2024-06-18 11:11:36 +0200

    Update ngrokk.sh

--- linux-scripts/ngrokk.sh
+++ linux-scripts/ngrokk.sh
@@ -3,6 +3,7 @@
 # Kill ngrok in Linux if not on a Windows machine
 if [ "$MOD_LOC_SCRIPT_ENVIRONMENT" != "windows" ]; then
     pkill ngrok
+    echo "ngrok has been killed."
 fi
 
 execute_part "deconfig-ngrok" "ngrok"
