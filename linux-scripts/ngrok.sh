d03a0f937411eee82fb95740e2051eb3e7f04f82
Oskar Modig <accounts@oskarmodig.com>
2024-06-18 10:38:40 +0200

    rewrite ngrok for linux

--- linux-scripts/ngrok.sh
+++ linux-scripts/ngrok.sh
@@ -1,77 +1,68 @@
-#!/usr/bin/env bash
-set -eo pipefail
+#!/bin/bash
 
-#
-# Share a Lando based WordPress website using ngrok
-# Cal Evans <cal@calevans.com>
-# https://hackernoon.com/lando-wordpress-and-ngrokoh-my
-#
-# This script must be run OUTSIDE of the Lando environment but in the Lando dir.
-# It uses the lando info command which is not available inside of the lando container.
-#
-# This is designed to work with a WordPress recipe. To make WordPress play nice
-# you have to install the plugin at https://github.com/jonathanbardo/WP-Ngrok-Local
-# Once you have that installed and activated, run this script and then visit
-# the https URL that it shows.
-#
-# When you are done, press CTRL+C to exit.
-#
-
-#
-# Check for sane environment
-#
-if ! command -v ngrok &> /dev/null
-then
-  echo ngrok is not installed.
-  echo Visit https://ngrok.com/download and download the version for your OS.
-  exit 1
+# Check if ngrok is installed
+if ! command -v ngrok &>/dev/null; then
+    echo "ngrok is not installed."
+    echo "Visit https://ngrok.com/download and download the version for your OS."
+    exit 1
 fi
 
-if ! command -v jq &> /dev/null
-then
-  echo jq is not installed.
-  echo Visit https://stedolan.github.io/jq/download/ and download the version for your OS.
-  echo If you have one, you can usually use your package manager to install it.
-  exit 2
+# Check if jq is installed
+if ! command -v jq &>/dev/null; then
+    echo "jq is not installed."
+    echo "Visit https://stedolan.github.io/jq/download/ and download the version for your OS."
+    echo "If you have one, you can usually use your package manager to install it."
+    exit 1
 fi
 
-if [ -z "$MODIG_NGROK_FULL_URL" ]; then
-    # Get the https url from lando
-    MODIG_NGROK_FULL_URL=$(lando info --format json | jq '.[0].urls[3]' | tr '"' ' ')
+# Check if MODIG_NGROK_FULL_LOCAL_URL is set and if not, get the URL from lando
+if [ -z "$MODIG_NGROK_FULL_LOCAL_URL" ]; then
+    MODIG_NGROK_FULL_LOCAL_URL=$(lando info --format json | jq -r '.[0].urls[3]')
 fi
 
+# Extract the protocol (http or https)
+PROTOCOL=${MODIG_NGROK_FULL_LOCAL_URL%%:*}
 
-#
-# Strip off the protocol
-#
-MODIG_NGROK_URL=$(echo "$MODIG_NGROK_FULL_URL" | awk -F '//' '{print $2}')
-MODIG_NGROK_URL=${MODIG_NGROK_URL::-1}
+# Remove any trailing slash
+MODIG_NGROK_FULL_LOCAL_URL="${MODIG_NGROK_FULL_LOCAL_URL%/}"
 
-#
-# Call ngrok
-#
-echo "Staring ngrok for $MODIG_NGROK_URL with name $MODIG_NGROK_FULL_URL"
-ngrok http --host-header="$MODIG_NGROK_URL" "$MODIG_NGROK_FULL_URL" > /dev/null &
+# Remove the protocol
+MODIG_LOCAL_URL_WITHOUT_PROTOCOL="${MODIG_NGROK_FULL_LOCAL_URL#*//}"
 
+# Check for a leading double slash (//) and remove it, if present
+if [[ "$MODIG_LOCAL_URL_WITHOUT_PROTOCOL" == //* ]]; then
+    MODIG_LOCAL_URL_WITHOUT_PROTOCOL="${MODIG_LOCAL_URL_WITHOUT_PROTOCOL:2}"
+fi
 
-# Give ngrok a few seconds to initialize
+echo "Starting ngrok for $MODIG_NGROK_FULL_LOCAL_URL"
+
+# Define initial flag variable
+FLAGS="--host-header=rewrite"
+
+if [ -n "$MODIG_NGROK_REMOTE_DOMAIN" ]; then
+    FLAGS="$FLAGS --domain=$MODIG_NGROK_REMOTE_DOMAIN"
+fi
+
+if [ -n "$MODIG_NGROK_OAUTH_GOOGLE" ]; then
+    FLAGS="$FLAGS --oauth=google"
+elif [ -n "$MODIG_NGROK_OAUTH_GOOGLE_DOMAIN" ]; then
+    FLAGS="$FLAGS --oauth=google --oauth-allow-domain=$MODIG_NGROK_OAUTH_GOOGLE_DOMAIN"
+elif [ -n "$MODIG_NGROK_OAUTH_GOOGLE_EMAIL" ]; then
+    FLAGS="$FLAGS --oauth=google --oauth-allow-email=$MODIG_NGROK_OAUTH_GOOGLE_EMAIL"
+fi
+
+# Start ngrok in the background
+ngrok http $FLAGS "$MODIG_NGROK_FULL_LOCAL_URL" &
+
+# Pause for 2 seconds to give ngrok time to start
 sleep 2
 
-# Extract protocol (http or https)
-protocol=$(echo "$MODIG_NGROK_FULL_URL" | grep -oE '^(http|https)')
-
-# Check if URL contains a port
-if [[ ! "$MODIG_NGROK_FULL_URL" =~ :[0-9]+ ]]; then
-    # No port found, add default port based on protocol
-    if [[ "$protocol" == "http" ]]; then
-        MODIG_NGROK_FULL_URL="${MODIG_NGROK_FULL_URL}:80"
-    elif [[ "$protocol" == "https" ]]; then
-        MODIG_NGROK_FULL_URL="${MODIG_NGROK_FULL_URL}:443"
-    fi
+if [ -n "$MODIG_NGROK_REMOTE_DOMAIN" ]; then
+    MODIG_NGROK_REMOTE_URL="$PROTOCOL://$MODIG_NGROK_REMOTE_DOMAIN"
+else
+    # Query the ngrok API for the tunnel information and parse it to get the public URL
+    MODIG_NGROK_REMOTE_URL=$(./get-ngrok-url.sh -url "$MODIG_NGROK_FULL_LOCAL_URL")
 fi
 
-# Filter tunnels to find one with the ConfigAddr matching MODIG_NGROK_FULL_URL and get its public_url
-# shellcheck disable=SC2034
-NGROK_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r --arg url "$MODIG_NGROK_FULL_URL" '.tunnels[] | select(.config.addr == $url) | .public_url | select(.!=null) | .')
-
-execute_part "config-ngrok"
+NGROK_URL="$MODIG_NGROK_REMOTE_URL"
+execute_part "config-ngrok" "ngrok"
