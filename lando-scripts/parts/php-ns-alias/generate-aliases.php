19ca9bfc817f4c670eb05bbc6bd2fea56d3a0e0c
Oskar Modig <accounts@oskarmodig.com>
2024-09-12 10:35:29 +0200

    ns generator fixes

--- lando-scripts/parts/php-ns-alias/generate-aliases.php
+++ lando-scripts/parts/php-ns-alias/generate-aliases.php
@@ -4,6 +4,8 @@
  * helping with loading Northmill Online Shared with different versions for different plugins/themes.
  *
  * @package Modig_Scripts
+ *
+ * @phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
  */
 
 /**
@@ -11,15 +13,16 @@
  *
  * @noinspection PhpConditionAlreadyCheckedInspection
  */
-if ( 4 !== $argc ) {
-	echo "Usage: php generate_aliases.php <directory> <original_namespace> <package_namespace>\n";
+if ( 5 !== $argc ) {
+	echo "Usage: php generate_aliases.php <output_directory> <directory> <original_namespace> <package_namespace>\n";
 	exit( 1 ); // Exit with an error code.
 }
 
 // Get the input arguments from the terminal.
-$directory          = $argv[1]; // The first argument is the directory.
-$original_namespace = $argv[2]; // The second argument is the original namespace.
-$package_namespace  = $argv[3]; // The third argument is the package namespace.
+$output_directory   = $argv[1]; // The first argument is the directory where the file will be created.
+$directory          = $argv[2]; // The first argument is the directory.
+$original_namespace = $argv[3]; // The second argument is the original namespace.
+$package_namespace  = $argv[4]; // The third argument is the package namespace.
 
 /**
  * Function to recursively scan the directory for PHP files.
@@ -55,6 +58,6 @@ function scan_directory( string $directory, string $original_namespace, string $
 $alias_content = scan_directory( $directory, $original_namespace, $package_namespace );
 
 // Write the alias statements to ide_helper.php.
-file_put_contents( __DIR__ . '/modlan_ns_alias_helper.php', "<?php\n//phpcs:disable\n" . $alias_content ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_file_put_contents
+file_put_contents( $output_directory . '/modlan_ns_alias_helper.php', "<?php\n//phpcs:disable\n" . $alias_content ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_file_put_contents
 
-echo "Class alias file generated successfully.\n";
+echo "Class alias file generated successfully in $output_directory.\n";
