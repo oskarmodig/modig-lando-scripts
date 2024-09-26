d3b164df798fd8644dd12e982545e00c01430b5a
Oskar Modig <accounts@oskarmodig.com>
2024-09-26 14:04:05 +0200

    Update co-up.sh

--- linux-scripts/co-up.sh
+++ linux-scripts/co-up.sh
@@ -1,9 +1,9 @@
 # Run composer update both on local machine and in the container
 composer update
 
-# If composer script "add-prefix" exists, run it
-if [ -f "composer.json" ] && [ -n "$(jq -r '.scripts."add-prefix"' composer.json)" ]; then
-    echo_progress "Running 'composer add-prefix'"
+# If composer script "scoper-prefix" exists, run it
+if [ -f "composer.json" ] && [ -n "$(jq -r '.scripts."scoper-prefix"' composer.json)" ]; then
+    echo_progress "Running 'composer scoper-prefix'"
     composer add-prefix
 fi
 
