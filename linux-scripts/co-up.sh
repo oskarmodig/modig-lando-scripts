e4bec3c8e550654ab9e0271cbc4e4caf0f02e0ce
Oskar Modig <accounts@oskarmodig.com>
2024-09-26 13:59:38 +0200

    add composer prefix

--- linux-scripts/co-up.sh
+++ linux-scripts/co-up.sh
@@ -1,3 +1,10 @@
 # Run composer update both on local machine and in the container
 composer update
+
+# If composer script "add-prefix" exists, run it
+if [ -f "composer.json" ] && [ -n "$(jq -r '.scripts."add-prefix"' composer.json)" ]; then
+    echo_progress "Running 'composer add-prefix'"
+    composer add-prefix
+fi
+
 lando composer update
