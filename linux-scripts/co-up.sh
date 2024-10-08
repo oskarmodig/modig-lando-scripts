d5f4c760f7eebb6019add373b747ce1569bf9c2e
Oskar Modig <accounts@oskarmodig.com>
2024-10-08 08:10:19 +0200

    Update co-up.sh

--- linux-scripts/co-up.sh
+++ linux-scripts/co-up.sh
@@ -4,6 +4,7 @@ composer update
 # If composer script "scoper-prefix" exists, run it
 if [ -f "composer.json" ] && [ -n "$(jq -r '.scripts."scoper-prefix"' composer.json)" ]; then
     echo_progress "Running 'composer scoper-prefix'"
+    rm -rf online-shared
     composer scoper-prefix
 fi
 
