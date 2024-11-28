69b4925a3d48249be75d4f293f1aa1555a696352
Oskar Modig <accounts@oskarmodig.com>
2024-11-28 10:54:04 +0100

    Update create-htaccess.sh

--- linux-scripts/parts/setup/create-htaccess.sh
+++ linux-scripts/parts/setup/create-htaccess.sh
@@ -32,19 +32,24 @@ RewriteRule . index.php [L]
     else
       # Commands for multisite with subdirectories .htaccess
       echo "Creating .htaccess for a multisite installation with subdirectories..."
-      echo "# BEGIN WordPress
+      echo "# BEGIN WordPress Multisite
+
 RewriteEngine On
+RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
 RewriteBase /
 RewriteRule ^index\.php$ - [L]
+
 # add a trailing slash to /wp-admin
-RewriteRule ^wp-admin$ wp-admin/ [R=301,L]
+RewriteRule ^([_0-9a-zA-Z-]+/)?wp-admin$ \$1wp-admin/ [R=301,L]
+
 RewriteCond %{REQUEST_FILENAME} -f [OR]
 RewriteCond %{REQUEST_FILENAME} -d
 RewriteRule ^ - [L]
 RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-(content|admin|includes).*) \$2 [L]
 RewriteRule ^([_0-9a-zA-Z-]+/)?(.*\.php)$ \$2 [L]
 RewriteRule . index.php [L]
-# END WordPress" | lando ssh -c "cat > $htaccess_path"
+
+# END WordPress Multisite" | lando ssh -c "cat > $htaccess_path"
     fi
   else
     # Commands for single site .htaccess
