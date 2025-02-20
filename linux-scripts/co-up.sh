0c15b21026a65b73f6e36a100313076f0be02262
Oskar Modig <accounts@oskarmodig.com>
2025-02-20 16:21:04 +0100

    no interaction and discard changes for scoper flow

--- linux-scripts/co-up.sh
+++ linux-scripts/co-up.sh
@@ -1,8 +1,11 @@
 # Run composer update both on local machine and in the container
-composer update
 
 # If composer script "scoper-prefix" exists, run it
 if [ -f "composer.json" ] && [ -n "$(jq -r '.scripts."scoper-prefix"' composer.json)" ]; then
+    # Running in no interaction mode with config to discard changes.
+    # This is since we remove php files from the vendor dir.
+    COMPOSER_DISCARD_CHANGES=true composer update -no-interaction
+
     echo_progress "Running 'composer scoper-prefix'"
     rm -rf online-shared
     composer scoper-prefix
@@ -10,6 +13,8 @@ if [ -f "composer.json" ] && [ -n "$(jq -r '.scripts."scoper-prefix"' composer.j
     echo_progress "Removing php files from online-shared vendor dir"
     find "vendor/northmill/online-shared" -type f -name "*.php" -exec rm -f {} +
     find "vendor/northmill/online-shared" -type d -empty -delete
+else
+    composer update
 fi
 
 lando composer install
